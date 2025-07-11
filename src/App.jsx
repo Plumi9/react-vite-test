0c812dca1f1a21104cd85070608802ef75a59444
Plumi9 <PhilipHoang02@gmail.com>
2025-07-11 23:29:53 +0200

    changed h1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>You gay heh</h1>
+      <h1>You gay hehheheheeh2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
